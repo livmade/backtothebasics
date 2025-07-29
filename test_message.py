02a15595316bf090df02d8084c04ee20fcd58998
Olivia Smith <oliviansmith@outlook.com>
2025-07-29 18:07:02 -0400

    working with strings

---
+++ test_message.py
@@ -0,0 +1,14 @@
+test_message = "This is a test message."
+def print_test_message():
+    print(test_message) 
+# This function prints a test message to the console
+
+print_test_message()
+
+test_message = "This is a modified test message."
+def print_modified_test_message():
+    print(test_message) 
+# This function prints a modified test message to the console
+
+
+print_modified_test_message()
